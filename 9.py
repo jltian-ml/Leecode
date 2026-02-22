ee912e2e9f3e22cba2126b2dcd055b314923799e
jltian-ml <2368439083@qq.com>
2026-02-22 16:15:40 +0000

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,10 @@
+ANSWER:
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+
+        s = str(x)
+        return s == s[::-1]
